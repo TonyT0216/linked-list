c3ab6bbb211dd749ff74d8f5b51a43dd39c166cd
Tony Toscano <tonymichael16@gmail.com>
2017-02-18 16:06:32 -0500

    Minor code changes to make sure each method was returning the right data, and also clarification of commenting within linked-list.js

--- scripts/linked-list.js
+++ scripts/linked-list.js
@@ -2,248 +2,245 @@
 /* A strict js version of a LinkedList class */
 class LinkedList {
 
-	// Constructor for LinkedList class
-	constructor(){
-		this.head = null;
-		this.tail = null;
-		this.count = 0;
-	}
+    // Constructor for LinkedList class
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
 
-	// size function
-	// returns the count property of this Linked List
-	size(){
-		return this.count;
-	}
+    /*
+    	Size function that returns the count of this LinkedList
+    	Returns this.count of the LinkedList  
+    */
+    size() {
+        return this.count;
+    }
 
-	// Add data to this LinkedList
-	// throws an error message if the argument is null
-	add(data){
+    /* 
+       Add the given data argument into this LinkedList 
+       Will throw an Error message if the argument is null 
+       or undefined
+       Returns +1 to indicate a successful addition to the list
+     */
+    add(data) {
 
-		if(data === null)
-		{
-			throw new Error("No data to put into the LinkedList class");
-		}
+        let itemAdded = -1;
 
-		let node = {
-			data: data,
-			next: null,
-		}
+        if (data === null || data === undefined) {
+            throw new Error("No data to put into the LinkedList class");
+        }
 
-		let currentNode = this.head;
-		if( !currentNode ){
-			this.head = node;
-			this.count++;
-			return node;
-		}
+        // let node = {
+        //     data: data,
+        //     next: null,
+        // }
 
-		while( currentNode.next ){
-			currentNode = currentNode.next;
-		}
+        let currentNode = this.head;
+        if (!currentNode) {
+            this.addFirst(data);
+        } else {
+            let node = {
+                data: data,
+                next: null,
+            }
 
-		currentNode.next = node;
-		this.count++;
-		return node;
-	}
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
 
-	// Add data argument into the LinkedList at the location index 
-	addAtIndex(data, index)
-	{
+            currentNode.next = node;
+            this.count++;
+            itemAdded = 1;
+        }
 
-		if( index < 0 ) {
-			throw new Error("Index must be at least 1");
-		}
+    }
 
-		else if( index > this.count )
-		{
-			throw new Error("The index is greater than the current size of the list.");
-		}
+    /*
+    	Adds an the given data argument into this LinkedList
+    	at the given index.
+    	Will throw an Error message if the index is less than
+    	zero, or if the index is greater than the size of this 
+    	LinkedList
+    	Doesn't return anything
+    */
+    addAtIndex(data, index) {
 
-		let tempNode = 
-		{
-			data: data,
-			next: null
-		}
+        if (index < 0) {
+            throw new Error("Index must be at least 1");
+        } else if (index > this.count) {
+            throw new Error("The index is greater than the current size of the list.");
+        }
 
-		let currentNode = this.head;
+        let tempNode = {
+            data: data,
+            next: null
+        }
 
-		if( index === 0 ){
-			this.addFirst(data);
-			return;
-		} else {
-			for(let i = 1; i < index; i++)
-			{
-				if(index === this.count)
-				{
-					this.addLast(data);
-					return;
-				}
-				currentNode = currentNode.next;
-			}
-			tempNode.next = currentNode.next;
-			currentNode.next = tempNode;
-		}
+        let currentNode = this.head;
 
-		this.count++;
-	}
+        if (index === 0) {
+            this.addFirst(data);
+        } else {
+            for (let i = 1; i < index; i++) {
+                if (index === this.count) {
+                    this.addLast(data);
+                }
+                currentNode = currentNode.next;
+            }
+            tempNode.next = currentNode.next;
+            currentNode.next = tempNode;
+        }
 
-	addFirst(data){
+        this.count++;
+    }
 
-		if(data === null)
-			return;
+    /*
+    	Add the data argument into this LinkedList as the first element.
+    	Will throw an Error message if the data is null or undefined
+    	Doesn't return anything
+    */
+    addFirst(data) {
 
-		let node = {
-			data: data,
-			next: null
-		}
+        if (data === null || data === undefined) {
+            throw new Error("No data to put into the LinkedList class");
+        }
 
-		if( this.count === 0 ){
-			this.head = node;
-			this.tail = node;
-			this.count++;
-		} else {
-			node.next = this.head;
-			this.head = node;
-			this.count++;
-		}
-	}
+        let node = {
+            data: data,
+            next: null
+        }
 
-	addLast(data){
+        if (this.count === 0) {
+            this.head = node;
+            this.tail = node;
+            this.count++;
+        } else {
+            node.next = this.head;
+            this.head = node;
+            this.count++;
+        }
+    }
 
-		if( data === null )
-			return;
+    /*
+    	Add the data argument into this LinkedList as the last element.
+    	Will throw an Error message if the data is null or undefined
+    	Doesn't return anything
+    */
+    addLast(data) {
 
-		let node = {
-			data: data,
-			next: null
-		}
+        if (data === null || data === undefined) {
+            throw new Error("No data to put into the LinkedList class");
+        }
 
-		if( this.count === 0 ){
-			this.addFirst(data);
-		} else {
-			this.tail.next = node;
-			this.tail = node;
-			this.count++;
-		}
-	}
+        let node = {
+            data: data,
+            next: null
+        }
 
-	contains(data)
-	{
-		if(data === null)
-			throw new Error("Null data not allowed");
+        if (this.count === 0) {
+            this.addFirst(data);
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+            this.count++;
+        }
+    }
 
-		let found = -1;
+    contains(data) {
+        if (data === null || data === undefined) {
+            throw new Error("Null data and undefined data is not present in the list.");
+        }
 
-		let currentLink = this.head;
+        let found = -1;
 
-		while( currentLink !== null && found === -1 ){
-			if( data === (currentLink.data)) {
-				found = 1;
-			}
-			else {
-				currentLink = currentLink.next;
-			}
-		}
-	  return found;
-	}
+        let currentLink = this.head;
 
-	get(index)
-	{
-		if( index >= this.count || index < 0)
-		{
-			throw new Error("The index doesn't make sense");
-		}
+        while (currentLink !== null && found === -1) {
+            if (data === (currentLink.data)) {
+                found = 1;
+            } else {
+                currentLink = currentLink.next;
+            }
+        }
+        return found;
+    }
 
-		let currentLink = this.head;
-		let count = 0;
-		while( currentLink !== null)
-		{
-			if( count === index )
-				return currentLink.data;
-			count++;
-			currentLink = currentLink.next;
-		}
+    get(index) {
+        if (index >= this.count || index < 0) {
+            throw new Error("The index doesn't make sense");
+        }
 
-		return -1;
-	}
+        let currentLink = this.head;
+        let count = 0;
+        while (currentLink !== null) {
+            if (count === index)
+                return currentLink.data;
+            count++;
+            currentLink = currentLink.next;
+        }
 
-	getNode(index)
-	{
-		if( index >= this.count || index < 0)
-		{
-			throw new Error("The index doesn't make sense");
-		}
+        return -1;
+    }
 
-		let currentLink = this.head;
-		let count = 0;
-		while( currentLink !== null )
-		{
-			if( count === index )
-				return currentLink;
-			count++;
-			currentLink = currentLink.next;
-		}
+    getNode(index) {
+        if (index >= this.count || index < 0) {
+            throw new Error("The index doesn't make sense");
+        }
 
-		return -1;
+        let currentLink = this.head;
+        let count = 0;
+        while (currentLink !== null) {
+            if (count === index)
+                return currentLink;
+            count++;
+            currentLink = currentLink.next;
+        }
 
-	}
+        return -1;
 
-	remove(data)
-	{
-		if(data === null)
-			throw new Error("Null data not allowed");
+    }
 
-		let found = -1;
+    remove(data) {
+        if (data === null)
+            throw new Error("Null data not allowed");
 
-		let previous = null;
-		let current = this.head;
+        let found = -1;
 
-		while ( current !== null && found === -1)
-		{
-			if( data === ( current.data )) {
-				found = 1;
-			}
-			else {
-				previous = current;
-				current = current.next;
-			}
-		}
+        let previous = null;
+        let current = this.head;
 
-		if(found === -1)
-			throw new Error("Element not found in the list");
+        while (current !== null && found === -1) {
+            if (data === (current.data)) {
+                found = 1;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+        }
 
-		if( this.size() === 1 )
-		{
-			this.head = this.tail = null;
-		}
-		else if( current === this.head)
-		{
-			this.head = current.next;
-		}
-		else if( current === this.tail )
-		{
-			this.tail = previous;
-			this.next = null;
-		}
-		else { 
-			previous.next = current.next;
-		}
+        if (found === -1)
+            throw new Error("Element not found in the list");
 
-		this.count--;
+        if (this.size() === 1) {
+            this.head = this.tail = null;
+        } else if (current === this.head) {
+            this.head = current.next;
+        } else if (current === this.tail) {
+            this.tail = previous;
+            this.next = null;
+        } else {
+            previous.next = current.next;
+        }
 
-		return current;
-	}
+        this.count--;
+
+        return current;
+    }
 }
 
 var linkedList = new LinkedList();
-linkedList.addFirst(3);
+// linkedList.add();
+linkedList.add(25);
 linkedList.addFirst(10);
-linkedList.addLast(20);
-linkedList.addAtIndex(100,2);
-linkedList.addAtIndex(150,3);
-linkedList.addAtIndex(300,5);
-linkedList.contains(100);
-linkedList.contains(5);
-linkedList.get(1);
-linkedList.remove(3);
-
-
+linkedList.addLast(12);
